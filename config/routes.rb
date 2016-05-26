365e615aa1422857393c260de5d6292e31410fd7
anoob <anoob.bava@gmail.com>
2016-05-26 19:57:55 +0530

    added the contacts page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :line_items
   resources :carts
   get 'store/index'
+  get 'store/contact'
 
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
