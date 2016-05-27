31e281706b87cc4dc4310e642f6be49d553654c7
anoob <anoob.bava@gmail.com>
2016-05-28 00:19:13 +0530

    completed the modal integration

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,12 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function(){
+  $('.form form-register-user').hide();
+  
+  $('.message-signin a').click(function() {
+    $('.form-login-user').hide();
+    $('.form-register-user').show();
+  });
+});
