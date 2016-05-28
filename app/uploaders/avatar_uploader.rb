989231926dc1d1cd77ded644352af9390a80260e
anoob <anoob.bava@gmail.com>
2016-05-28 16:04:55 +0530

    completed the bootrap functionality to products new page

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -33,11 +33,11 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [50, 50]
+    process :resize_to_fit => [125, 125]
   end
 
   version :medium do
-    process :resize_to_fit => [100, 100]
+    process :resize_to_fit => [250, 300]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
